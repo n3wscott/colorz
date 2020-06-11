0a8f94de09c54da2d68d3d912fbcb1c23cbfbbbb
Scott Nichols <snichols@vmware.com>
2020-06-11 11:13:06 -0700

    simple demo app in nodejs

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+const SERVICE = process.env.K_SERVICE || "unknown";
+const REVSION = process.env.K_REVISION || "unknown";
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send(SERVICE + '@' + REVSION);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
